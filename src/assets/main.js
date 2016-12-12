c261693b809a0e17d84bf4254d1a26f8acd77012
Corey Poff <cpoff@therues.com>
2016-12-12 16:46:33 -0500

    Ready for review

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/CoreyP.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      console.log('response: ', response.courses.completed);
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      //append course to parent div (badges)
+      var $course = $('<div />', { 'class': 'course' }).appendTo(badges);
+
+      //append title
+      $('<h3 />', { text: course.title }).appendTo($course);
+
+      //append image
+      $('<img />', { src: course.badge }).appendTo($course);
+
+      //append button
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+
+    });
+
+  }
 
 });
